fe32dcdc449248614a1a2d3cbe4a83d4662324f1
ueki05 <uekiyuta1991@gmail.com>
2017-02-20 23:13:28 +0900

    end 14.2.4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# ユーザー
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -14,8 +15,17 @@ User.create!(name:  "Example User",
                password_confirmation: password)
 end
 
+# マイクロソフト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
